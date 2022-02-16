699c28affc68d133f8153f660cc87a23f276ae7a
siweh <sanemadesi@gmail.com>
2022-02-16 22:39:42 +0200

    Done with styling and adding a todo

---
+++ app.js
@@ -0,0 +1,27 @@
+//Selectors
+document.querySelector('form').addEventListener('submit', handleSubmitForm);
+
+//Event handler function
+function handleSubmitForm(e) {
+  e.preventDefault();
+  let input = document.querySelector('input');
+  if (input.value != '') {
+    addTodo(input.value);
+  }
+  input.value = '';
+}
+
+//Add a todo function
+function addTodo(todo) {
+  let ul = document.querySelector('ul');
+  let li = document.createElement('li');
+
+  li.innerHTML = `
+    <span class="todo-item">${todo}</span>
+    <button name="checkButton"><i class="fas fa-check-square"></i></button>
+    <button name="deleteButton"><i class="fas fa-trash"></i></button>
+    `;
+
+  li.classList.add('todo-list-item');
+  ul.appendChild(li);
+}
